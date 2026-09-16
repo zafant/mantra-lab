213d89a02f4221e391362fb777dc622f6901bee2
zafant <77676734+zafant@users.noreply.github.com>
2026-09-16 23:19:52 +0200

    fix: refresh PWA cache for listone and builder v34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mantra-lab-v33';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./styles-v32.css','./styles-v33.css','./app.js','./manifest.webmanifest','./icon.svg']))));
+const CACHE='mantra-lab-v34';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./styles-v32.css','./styles-v33.css','./styles-v34.css','./app.js','./data-provider.js','./data/listone-2026-27.csv','./manifest.webmanifest','./icon.svg']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
