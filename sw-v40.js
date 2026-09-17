e5f9a5eb056ec3c7b30af8fbc7ed3691312724b4
zafant <77676734+zafant@users.noreply.github.com>
2026-09-17 23:05:20 +0200

    fix: refresh V4.4 module runtime

--- sw-v40.js
+++ sw-v40.js
@@ -1,4 +1,4 @@
-const CACHE='mantra-lab-v43';
+const CACHE='mantra-lab-v44';
 const ASSETS=['./','./index.html','./styles.css','./styles-v32.css','./styles-v34.css','./styles-v40.css?v=42','./app-v40.js?v=42','./data-provider.js?v=42','./data/listone-2026-27.csv','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
